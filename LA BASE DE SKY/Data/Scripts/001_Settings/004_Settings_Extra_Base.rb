5698673e3704992fce13c20b6d40e42f9f77beab
Skyflyer <157004728+SkyFangames@users.noreply.github.com>
2026-04-06 22:27:19 +0200

    Mueve settings de las sombras al archivo settings

--- LA BASE DE SKY/Data/Scripts/001_Settings/004_Settings_Extra_Base.rb
+++ LA BASE DE SKY/Data/Scripts/001_Settings/004_Settings_Extra_Base.rb
@@ -463,3 +463,44 @@ module DamageNumberSettings
   DURATION    = 50
   FLOAT_DIST  = 60
 end
+
+
+
+module OWShadowSettings
+  # If true, calculates the shadow size based on the sprite's pixels.
+  # If false, uses the size defined in FIXED_SHADOW_SIZE for all.
+  AUTOMATIC_SHADOW_GENERATION = false
+
+  # Fixed shadow size when automatic generation is disabled (false).
+  # A value of 10 to 14 is standard for normal-sized characters.
+  FIXED_SHADOW_SIZE = 12
+
+  # ============================================================================
+  # Set this to true if you want the event name and character name blacklists to be case sensitive.
+  CASE_SENSITIVE_BLACKLISTS = false
+
+  # If an event name contains one of these words, it will not have a shadow.
+  SHADOWLESS_EVENT_NAME     = [
+    "door", "FlechaSalida", "nurse", "Enfermera", "Healing balls", "Balls curativas", "Mart","Tendero", "SmashRock", "RocaRompible", "StrengthBoulder", "PiedraFuerza",
+    "CutTree", "ArbolCorte", "HeadbuttTree", "ArbolGolpeCabeza", "BerryPlant", "Planta Bayas", ".shadowless", ".noshadow", ".sl", "Entrada Mazmorra Bosque", "Entrada Cueva", "Relic Stone",
+    "Escalera", "Puerta", "ExitArrow", "NoShadow"
+  ]
+
+  # If the character file and event uses contains one of these words in its filename, it will not have a shadow.
+  SHADOWLESS_CHARACTER_NAME = ["nil"]
+
+  # If an event stands on a tile with one of these terrain tags, it will not have a shadow.
+  # (Names can be seen in the script section "Terrain Tag")
+  SHADOWLESS_TERRAIN_NAME   = [
+    :Grass, :DeepWater, :StillWater, :Water, :Waterfall, :WaterfallCrest,
+    :Puddle
+  ]
+
+  # Hash to adjust the shadow radius for specific character files.
+  # Key: Part of the filename (e.g., "PIKACHU").
+  # Value: [RADIUS, X, Y]
+  CHARACTER_SHADOW_FIX = {
+    "PIKACHU" => [-2, 0, 0],
+    "SNORLAX" => [8, 0, 0],
+  }
+end
