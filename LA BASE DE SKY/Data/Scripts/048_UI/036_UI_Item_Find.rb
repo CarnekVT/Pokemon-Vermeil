6a9b617f2f1fea4e817e86dc7dc32e0556391133
Diego Pertierra <dpertierra@hotmail.com>
2026-04-30 20:04:46 +0200

    El item find ahora para MTs muestra la descripción del movimiento

--- LA BASE DE SKY/Data/Scripts/048_UI/036_UI_Item_Find.rb
+++ LA BASE DE SKY/Data/Scripts/048_UI/036_UI_Item_Find.rb
@@ -8,9 +8,9 @@ if Settings::SHOW_ITEM_DESCRIPTIONS_ON_RECEIVE
   # A script to show a helpful message with item name, icon and description
   # when an item is found for the first time.
   #-------------------------------------------------------------------------------
-  
+
   WINDOWSKIN_NAME = "" # set for custom windowskin
-  
+
   #-------------------------------------------------------------------------------
   # Save data registry
   #-------------------------------------------------------------------------------
@@ -19,13 +19,13 @@ if Settings::SHOW_ITEM_DESCRIPTIONS_ON_RECEIVE
     load_value { |value| $item_log = value }
     new_game_value { ItemLog.new }
   end
-  
+
   #-------------------------------------------------------------------------------
   # Base Class
   #-------------------------------------------------------------------------------
-  
+
   class PokemonItemFind_Scene
-    
+
     ITEM_ICON_X = 42
     ITEM_ICON_Y_OFFSET = -48
     DESC_WINDOW_X = 64
@@ -43,68 +43,65 @@ if Settings::SHOW_ITEM_DESCRIPTIONS_ON_RECEIVE
       @sprites = {}
       skin = WINDOWSKIN_NAME == "" ? MessageConfig.pbGetSystemFrame : "Graphics/Windowskins/" + WINDOWSKIN_NAME
       
-      
       @sprites["background"] = Window_UnformattedTextPokemon.newWithSize("", 0, 0, Graphics.width, 0, @viewport)
       @sprites["background"].z = @viewport.z - 1
       @sprites["background"].visible = false
-  
-  
+
       @sprites["background"].setSkin(skin)
-      
+
       colors = getDefaultTextColors(@sprites["background"].windowskin)
-  
+
       @sprites["itemicon"] = ItemIconSprite.new(ITEM_ICON_X, Graphics.height + ITEM_ICON_Y_OFFSET, item.id, @viewport)
       @sprites["itemicon"].visible = false
       @sprites["itemicon"].z = @viewport.z + 2
-      
-      
+
       @sprites["descwindow"] = Window_UnformattedTextPokemon.newWithSize("", DESC_WINDOW_X, DESC_WINDOW_Y, Graphics.width + DESC_WINDOW_WIDTH_OFFSET, DESC_WINDOW_HEIGHT, @viewport)
       @sprites["descwindow"].windowskin = nil
       @sprites["descwindow"].z = @viewport.z
       @sprites["descwindow"].visible = false
       @sprites["descwindow"].baseColor = colors[0]
       @sprites["descwindow"].shadowColor = colors[1]
-  
+
       @sprites["titlewindow"] = Window_UnformattedTextPokemon.newWithSize("", TITLE_WINDOW_X, TITLE_WINDOW_Y, TITLE_WINDOW_WIDTH, TITLE_WINDOW_HEIGHT, @viewport)
       @sprites["titlewindow"].visible = false
       @sprites["titlewindow"].z = @viewport.z + 1
       @sprites["titlewindow"].windowskin = nil
       @sprites["titlewindow"].baseColor = colors[0]
       @sprites["titlewindow"].shadowColor = colors[1]
-  
     end
-  
+
     def pbShow(item)
       item_object = GameData::Item.try_get(item)
       name = item_object.name
+      description = item_object.description
       if item_object.is_machine?
         machine = GameData::Item.get(item_object).move
         name = _INTL("{1} {2}", name, GameData::Move.get(machine).name)
+        description = GameData::Move.get(machine).description
       end
-      description = item_object.description
-  
+
       descwindow = @sprites["descwindow"]
       descwindow.resizeToFit(description, Graphics.width + DESC_WINDOW_WIDTH_OFFSET)
       descwindow.text = description
       descwindow.y = Graphics.height - descwindow.height
       descwindow.visible = true
-  
+
       titlewindow = @sprites["titlewindow"]
       titlewindow.resizeToFit(name, Graphics.height)
       titlewindow.text = name
       titlewindow.y = Graphics.height - descwindow.height - TITLE_WINDOW_HEIGHT * 2
       titlewindow.visible = true
-  
+
       background = @sprites["background"]
       background.height = descwindow.height + DESC_WINDOW_HEIGHT / 2
       background.y = Graphics.height - background.height
       background.visible = true
-  
+
       itemicon = @sprites["itemicon"]
       itemicon.item = item
       itemicon.y = Graphics.height - (descwindow.height / 2).floor
       itemicon.visible = true
-  
+
       loop do
         background.update
         itemicon.update
@@ -119,14 +116,14 @@ if Settings::SHOW_ITEM_DESCRIPTIONS_ON_RECEIVE
         end
       end
     end
-  
+
     def pbEndScene
       pbDisposeSpriteHash(@sprites)
       @viewport.dispose
     end
   end
-  
-  
+
+
   #-------------------------------------------------------------------------------
   # Item Log class
   #-------------------------------------------------------------------------------
@@ -136,7 +133,7 @@ if Settings::SHOW_ITEM_DESCRIPTIONS_ON_RECEIVE
     def initialize()
       @found_items = []
     end
-  
+
     def register(item)
       if !@found_items.include?(item)
         @found_items.push(item)
@@ -147,27 +144,27 @@ if Settings::SHOW_ITEM_DESCRIPTIONS_ON_RECEIVE
       end
     end
   end
-  
+
   #-------------------------------------------------------------------------------
   # Overrides of pbItemBall and pbReceiveItem
   #-------------------------------------------------------------------------------
   # Picking up an item found on the ground
   #-------------------------------------------------------------------------------
-  
+
   alias pbItemBall_itemfind pbItemBall
   def pbItemBall(item, quantity = 1, outfit = nil)
     result = pbItemBall_itemfind(item, quantity, outfit)
     $item_log.register(item) if result
     return result
   end
-  
+
   alias pbReceiveItem_itemfind pbReceiveItem
   def pbReceiveItem(item, quantity = 1, outfit_change = nil)
     result = pbReceiveItem_itemfind(item, quantity, outfit_change)
     $item_log.register(item) if result
     return result
   end
-  
+
   alias pbPickBerry_itemfind pbPickBerry
   def pbPickBerry(berry, qty = 1)
     ret = pbPickBerry_itemfind(berry,qty)
