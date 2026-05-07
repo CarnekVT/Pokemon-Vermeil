d11802f329582e14bdc0b3d991d54de84ba34feb
CarnekVT <carmanekkodeviant@gmail.com>
2026-05-07 17:00:50 -0500

    Cleaning Base

--- Plugins/HGSS Multi-Save/001_Technical/003_Intl_Messages.rb
+++
@@ -1,8 +0,0 @@
-#===============================================================================
-#
-#===============================================================================
-class Translation
-  def self.month_day_date_format?
-    return System.user_language[3..4] == "US"   # If the user is in the United States
-  end
-end
