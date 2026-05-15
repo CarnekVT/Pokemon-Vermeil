5222bd1bed54146147140fc41913d78846b4a34f
Diego Pertierra <dpertierra@hotmail.com>
2026-05-15 21:55:38 +0200

    Correccion dado trucado de nuevo

--- LA BASE DE SKY/Data/Scripts/030_Move/010_MoveEffects_MultiHit.rb
+++ LA BASE DE SKY/Data/Scripts/030_Move/010_MoveEffects_MultiHit.rb
@@ -124,6 +124,7 @@ class Battle::Move::HitTwoToFiveTimes < Battle::Move
   def multiHitMove?; return true; end
 
   def pbNumHits(user, targets)
+    return 4 + @battle.pbRandom(2) if user.hasActiveItem?(:LOADEDDICE)
     hitChances = [
       2, 2, 2, 2, 2, 2, 2,
       3, 3, 3, 3, 3, 3, 3,
@@ -133,7 +134,6 @@ class Battle::Move::HitTwoToFiveTimes < Battle::Move
     r = @battle.pbRandom(hitChances.length)
     r = hitChances.length - 1 if user.hasActiveAbility?(:SKILLLINK)
     ret = hitChances[r]
-    ret = 4 + @battle.pbRandom(2) if ret < 4 && user.hasActiveItem?(:LOADEDDICE)
     return ret
   end
 end
