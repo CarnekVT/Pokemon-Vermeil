090b9e79ad4317cfed2cb33669d57edc0390caf2
Diego Pertierra <dpertierra@hotmail.com>
2026-06-19 00:52:28 +0200

    Mueve los colores de los textos a una constante para que se puedan cambiar más facilmente en plugins

--- LA BASE DE SKY/Data/Scripts/018_Objects and windows/003_MessageConfig.rb
+++ LA BASE DE SKY/Data/Scripts/018_Objects and windows/003_MessageConfig.rb
@@ -27,6 +27,46 @@ module MessageConfig
   CURSOR_POSITION          = 1
   WINDOW_OPACITY           = 255
   TEXT_SPEED               = nil   # Time in seconds between two characters
+
+  TEXTCOLORS = [
+    [  0, 112, 248], [120, 184, 232],   # 1  Blue
+    [232,  32,  16], [248, 168, 184],   # 2  Red
+    [ 96, 176,  72], [174, 208, 144],   # 3  Green
+    [ 72, 216, 216], [168, 224, 224],   # 4  Cyan
+    [208,  56, 184], [232, 160, 224],   # 5  Magenta
+    [176, 143,  93], [235, 235,  44],   # 6  Yellow
+    [160, 160, 168], [208, 208, 216],   # 7  Gray
+    [240, 240, 248], [120, 120, 128],   # 8  White
+    [114,  64, 232], [184, 168, 224],   # 9  Purple
+    [248, 152,  24], [248, 200, 152],   # 10 Orange
+    MessageConfig::DARK_TEXT_MAIN_COLOR,
+    MessageConfig::DARK_TEXT_SHADOW_COLOR,   # 11 Dark default
+    MessageConfig::LIGHT_TEXT_MAIN_COLOR,
+    MessageConfig::LIGHT_TEXT_SHADOW_COLOR,   # 12 Light default
+    [120, 120, 128], [240, 240, 248],   # 13  White with black background
+    [138,  90,  43], [216, 184, 154],   # 14 Brown
+    [240,  96, 144], [248, 184, 200],   # 15 Pink
+    [152, 224,  32], [216, 240, 168],   # 16 Lime
+    [ 32,  56, 112], [144, 168, 216],   # 17 Navy
+    [140,  32,  32], [216, 160, 160],   # 18 Maroon
+    [ 32, 128, 112], [152, 200, 184],   # 19 Teal
+    [ 88, 168, 240], [184, 216, 248],   # 20 Sky Blue
+    [128, 128,  32], [200, 200, 160],   # 21 Olive
+    [224, 200, 160], [248, 232, 208],   # 22 Beige
+    [184, 144, 224], [224, 208, 248],   # 23 Lavender
+    [216, 176,  32], [240, 224, 168],   # 24 Gold
+    [240, 160,  32], [248, 216, 168],   # 25 Amber
+    [248, 160, 144], [248, 216, 200],   # 26 Peach
+    [200,  32,  48], [240, 160, 168],   # 27 Crimson
+    [248, 120,  88], [248, 200, 184],   # 28 Coral
+    [ 64,  64, 168], [184, 184, 232],   # 29 Indigo
+    [ 96, 120, 160], [192, 208, 232],   # 30 Slate Blue
+    [ 96, 216, 168], [200, 240, 224],   # 31 Mint
+    [ 32,  96,  32], [152, 192, 152],   # 32 Forest Green
+    [112,  32,  64], [200, 160, 184],   # 33 Plum
+    [168, 144, 120], [224, 208, 200],   # 34 Warm Gray
+    [128, 144, 168], [208, 216, 232]    # 35 Cool Gray
+  ]
   @@systemFrame     = nil
   @@defaultTextSkin = nil
   @@textSpeed       = nil
@@ -349,45 +389,7 @@ def get_text_colors_for_windowskin(windowskin, color, isDarkSkin, no_ctag = true
   end
     # Base color, shadow color (these are reversed on dark windowskins)
     # Values in arrays are RGB numbers
-    textcolors = [
-      [  0, 112, 248], [120, 184, 232],   # 1  Blue
-      [232,  32,  16], [248, 168, 184],   # 2  Red
-      [ 96, 176,  72], [174, 208, 144],   # 3  Green
-      [ 72, 216, 216], [168, 224, 224],   # 4  Cyan
-      [208,  56, 184], [232, 160, 224],   # 5  Magenta
-      [232, 208,  32], [248, 232, 136],   # 6  Yellow
-      [160, 160, 168], [208, 208, 216],   # 7  Gray
-      [240, 240, 248], [120, 120, 128],   # 8  White
-      [114,  64, 232], [184, 168, 224],   # 9  Purple
-      [248, 152,  24], [248, 200, 152],   # 10 Orange
-      MessageConfig::DARK_TEXT_MAIN_COLOR,
-      MessageConfig::DARK_TEXT_SHADOW_COLOR,   # 11 Dark default
-      MessageConfig::LIGHT_TEXT_MAIN_COLOR,
-      MessageConfig::LIGHT_TEXT_SHADOW_COLOR,   # 12 Light default
-      [120, 120, 128], [240, 240, 248],   # 13  White with black background
-      [138,  90,  43], [216, 184, 154],   # 14 Brown
-      [240,  96, 144], [248, 184, 200],   # 15 Pink
-      [152, 224,  32], [216, 240, 168],   # 16 Lime
-      [ 32,  56, 112], [144, 168, 216],   # 17 Navy
-      [140,  32,  32], [216, 160, 160],   # 18 Maroon
-      [ 32, 128, 112], [152, 200, 184],   # 19 Teal
-      [ 88, 168, 240], [184, 216, 248],   # 20 Sky Blue
-      [128, 128,  32], [200, 200, 160],   # 21 Olive
-      [224, 200, 160], [248, 232, 208],   # 22 Beige
-      [184, 144, 224], [224, 208, 248],   # 23 Lavender
-      [216, 176,  32], [240, 224, 168],   # 24 Gold
-      [240, 160,  32], [248, 216, 168],   # 25 Amber
-      [248, 160, 144], [248, 216, 200],   # 26 Peach
-      [200,  32,  48], [240, 160, 168],   # 27 Crimson
-      [248, 120,  88], [248, 200, 184],   # 28 Coral
-      [ 64,  64, 168], [184, 184, 232],   # 29 Indigo
-      [ 96, 120, 160], [192, 208, 232],   # 30 Slate Blue
-      [ 96, 216, 168], [200, 240, 224],   # 31 Mint
-      [ 32,  96,  32], [152, 192, 152],   # 32 Forest Green
-      [112,  32,  64], [200, 160, 184],   # 33 Plum
-      [168, 144, 120], [224, 208, 200],   # 34 Warm Gray
-      [128, 144, 168], [208, 216, 232]    # 35 Cool Gray
-    ]
+    textcolors = MessageConfig::TEXTCOLORS
     if color == 0 || color > textcolors.length / 2   # No special colour, use default
       if isDarkSkin   # Dark background, light text
         return no_ctag ? [MessageConfig::LIGHT_TEXT_MAIN_COLOR, MessageConfig::LIGHT_TEXT_SHADOW_COLOR] : shadowc3tag(MessageConfig::LIGHT_TEXT_MAIN_COLOR, MessageConfig::LIGHT_TEXT_SHADOW_COLOR)
