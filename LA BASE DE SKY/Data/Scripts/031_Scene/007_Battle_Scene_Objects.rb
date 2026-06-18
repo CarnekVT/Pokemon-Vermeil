a43a39bef20b42a70094375c84b9236a59192393
Diego Pertierra <dpertierra@hotmail.com>
2026-06-19 00:53:46 +0200

    Corrige error envenenamiento grave

--- LA BASE DE SKY/Data/Scripts/031_Scene/007_Battle_Scene_Objects.rb
+++ LA BASE DE SKY/Data/Scripts/031_Scene/007_Battle_Scene_Objects.rb
@@ -411,7 +411,7 @@ class Battle::Scene::PokemonDataBox < Sprite
   def draw_status
     return if @battler.status == :NONE
     if @battler.status == :POISON && @battler.statusCount > 0   # Badly poisoned
-      s = GameData::Status.count - 1
+      s = GameData::Status::DATA.values.map(&:icon_position).max + 1
     else
       s = GameData::Status.get(@battler.status).icon_position
     end
