1897b1a05cf40fd03665c93e4532f41aad01c4d5
Diego Pertierra <dpertierra@hotmail.com>
2026-07-07 22:55:35 +0200

    Corrige skip text

--- LA BASE DE SKY/Data/Scripts/018_Objects and windows/012_Messages.rb
+++ LA BASE DE SKY/Data/Scripts/018_Objects and windows/012_Messages.rb
@@ -759,6 +759,15 @@ def pbMessageDisplayFrame(msgwindow, controls, letterbyletter, commandProc, stat
     elsif !state[:appear_timer_start]
       return true
     end
+  elsif Input.press?(Input::ACTION) && ( $DEBUG || (Settings::ENABLE_SKIP_TEXT && $PokemonSystem.skip_texts==0))
+    msgwindow.textspeed=-999
+      msgwindow.update
+      if msgwindow.busy?
+        pbPlayDecisionSE() if msgwindow.pausing?
+        msgwindow.resume
+      elsif !state[:appear_timer_start]
+        return true
+      end
   end
   pbUpdateSceneMap
   msgwindow.update
